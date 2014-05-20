84e7f92e01c164a5012de6357b76fa6633884e75
unknown <jbradshaw@SL-WS-212.scottlogic.co.uk>
2014-05-20 15:03:16 +0100

    Extracted double hashing into function

--- Task1.js
+++ Task1.js
@@ -5,7 +5,7 @@ var crypto = require('crypto');
 var genesisBlockId = '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f';
 var exampleBlockId = '00000000000000001e8d6829a8a21adc5d38d0a473b144b6765798e61f98bd1d';
 
-getBlock(exampleBlockId, hashBlock);
+getBlock(genesisBlockId, hashBlock);
 
 function getBlock(blockId, callback)
 {
@@ -58,28 +58,47 @@ function hashBlock(block)
     rootHash = swapEndian(block.mrkl_root),
     time = dec2hex(block.time),
     bits = dec2hex(block.bits),
-    nonce = dec2hex(block.nonce);
-	
-	/*
-	var version =  '01000000',
-    prevBlockHash = '81cd02ab7e569e8bcd9317e2fe99f2de44d49ab2b8851ba4a308000000000000',
-    rootHash = 'e320b6c2fffc8d750423db8b1eb942ae710e951ed797f7affc8892b0f1fc122b',
-    time = 'c7f5d74d', 
-    bits = 'f2b9441a',
-    nonce = '42a14695';
-	*/
+    nonce = dec2hex(block.nonce);	
 	
 	var headerHex = version + prevBlockHash + rootHash + time + bits + nonce;
-	var headerBin = (new Buffer(headerHex, 'hex')).toString('binary');
+	
+	var hash = doubleHashHex(headerHex);
+	
+	console.log('HASH: ' + swapEndian(hash));	
+}
+
+/*
+* Performs a double hash on input hexadecimal string
+*/
+function doubleHashHex(hex)
+{
+	var bin = (new Buffer(hex, 'hex')).toString('binary');
 	
 	var hasher1 = crypto.createHash('sha256');
 	var hasher2 = crypto.createHash('sha256');
 	
-	hasher1.update(headerBin, 'binary');
+	hasher1.update(bin, 'binary');
 	
 	var hash1 = hasher1.digest('binary');
 	
 	hasher2.update(hash1, 'binary');
+	
 	var hash2 = hasher2.digest('hex');
-	console.log('HASH: ' + swapEndian(hash2));	
+	return hash2;	
 }
+
+function calulateMerkleRoot(block)
+{
+	
+}
+
+function formBottomRow(transactions)
+{
+
+}
+
+function formNextTreeRow(previousRow)
+{
+	
+}
+
